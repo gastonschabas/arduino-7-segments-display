cf35cd4f4eba13511aa180637f78fddc3d2684df
Gastón Schabas <gastonschabas@gmail.com>
2021-09-11 11:26:42 -0300

    Refactored code from common Anode to common Cathode

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # Arduino 7 segments display counter
 
-An Arduino UNO connected to a Anode 7 Segment Display. Once it starts, it will show the numbers from 0 to 9
+An Arduino UNO connected to a Cathode 7 Segment Display. Once it starts, it will show the numbers from 0 to 9
 
 # Components
 
 * 1 Arduino Uno R3
-* 1 Anode 7 Segment Display
+* 1 Cathode 7 Segment Display
 * 1 Resistors 330Ω
 
 # Circuit
 
 Once the circuit is turned on, it will show in the display the numbers from 0 to 9 in an infinite loop.
 
-![Circuit Preview](doc/arduino-anode-7-segments-display.png)
+![Circuit Preview](doc/arduino-cathode-7-segments-display.png)
 
 # Code
 
@@ -20,5 +20,5 @@ The code for Arduino can be found in the `src` directory. It's just a single fil
 
 # Simulation
 
-[watch how it works in tinkercad](https://www.tinkercad.com/things/koUVkOZHEKq)
+[watch how it works in tinkercad](https://www.tinkercad.com/things/f7kjUjaAmvP)
 
--- src/main.ino
+++ src/main.ino
@@ -1,4 +1,4 @@
-// Common Anode
+// Common Cathode
 //
 //       A
 //   ---------
@@ -11,106 +11,155 @@
 //   |   D   |
 //   ---------
 //       
-// 
 
-byte segmentA = 0b01111111;
-byte segmentB = 0b10111111;
-byte segmentC = 0b11011111;
-byte segmentD = 0b11101111;
-byte segmentE = 0b11110111;
-byte segmentF = 0b11111011;
-byte segmentG = 0b11111101;
-byte dot      = 0b11111110;
+byte segmentA = 2;
+byte segmentB = 3;
+byte segmentC = 4;
+byte segmentD = 5;
+byte segmentE = 6;
+byte segmentF = 7;
+byte segmentG = 8;
+byte dot      = 9;
 
-int t = 800;
 
-byte zero = segmentA
-    & segmentB
-    & segmentC
-    & segmentD
-    & segmentE
-    & segmentF;
-
-byte one = segmentB 
-    & segmentC; 
-
-byte two = segmentA 
-    & segmentB
-    & segmentG
-    & segmentE
-    & segmentD; 
-
-byte three = segmentA 
-    & segmentB
-    & segmentG
-    & segmentC
-    & segmentD; 
-
-byte four = segmentB
-    & segmentG
-    & segmentF
-    & segmentC; 
-
-byte five = segmentA 
-    & segmentF
-    & segmentG
-    & segmentC
-    & segmentD; 
-
-byte six = segmentA
-    & segmentG
-    & segmentC
-    & segmentD
-    & segmentE
-    & segmentF;
-
-byte seven = segmentA
-    & segmentB 
-    & segmentC; 
-
-byte eight = segmentA
-    & segmentB
-    & segmentC
-    & segmentD
-    & segmentE
-    & segmentF
-    & segmentG;
-
-byte nine = segmentA
-    & segmentB
-    & segmentC
-    & segmentD
-    & segmentF
-    & segmentG;
-
+short t = 750;
 
 void setup() {
-  DDRD = 0b11111111;
+  DDRD = DDRD | 0b11111100;
+  DDRB = DDRB | 0b00000011;
+  PORTD = 0b11111100;
+  PORTB = 0b00000011;
 }
 
 void loop() {
-  setPortDValue(zero);
+  zero();
   delay(t);
-  setPortDValue(one);
+  one();
   delay(t);
-  setPortDValue(two);
+  two();
   delay(t);
-  setPortDValue(three);
+  three();
   delay(t);
-  setPortDValue(four);
+  four();
   delay(t);
-  setPortDValue(five);
+  five();
   delay(t);
-  setPortDValue(six);
+  six();
   delay(t);
-  setPortDValue(seven);
+  seven();
   delay(t);
-  setPortDValue(eight);
+  eight();
   delay(t);
-  setPortDValue(nine);
+  nine();
   delay(t);
 }
 
-void setPortDValue(byte segments) {
-  PORTD = segments;
+void zero() {
+  digitalWrite(segmentA, HIGH);
+  digitalWrite(segmentB, HIGH);
+  digitalWrite(segmentC, HIGH);
+  digitalWrite(segmentD, HIGH);
+  digitalWrite(segmentE, HIGH);
+  digitalWrite(segmentF, HIGH);
+  digitalWrite(segmentG, LOW);
+  digitalWrite(dot, LOW);
+}
+
+void one() {
+  digitalWrite(segmentA, LOW);
+  digitalWrite(segmentB, HIGH);
+  digitalWrite(segmentC, HIGH);
+  digitalWrite(segmentD, LOW);
+  digitalWrite(segmentE, LOW);
+  digitalWrite(segmentF, LOW);
+  digitalWrite(segmentG, LOW);
+  digitalWrite(dot, LOW);
+}
+
+void two() {
+  digitalWrite(segmentA, HIGH);
+  digitalWrite(segmentB, HIGH);
+  digitalWrite(segmentC, LOW);
+  digitalWrite(segmentD, HIGH);
+  digitalWrite(segmentE, HIGH);
+  digitalWrite(segmentF, LOW);
+  digitalWrite(segmentG, HIGH);
+  digitalWrite(dot, LOW);
+}
+
+void three() {
+  digitalWrite(segmentA, HIGH);
+  digitalWrite(segmentB, HIGH);
+  digitalWrite(segmentC, HIGH);
+  digitalWrite(segmentD, HIGH);
+  digitalWrite(segmentE, LOW);
+  digitalWrite(segmentF, LOW);
+  digitalWrite(segmentG, HIGH);
+  digitalWrite(dot, LOW);
+}
+
+void four() {
+  digitalWrite(segmentA, LOW);
+  digitalWrite(segmentB, HIGH);
+  digitalWrite(segmentC, HIGH);
+  digitalWrite(segmentD, LOW);
+  digitalWrite(segmentE, LOW);
+  digitalWrite(segmentF, HIGH);
+  digitalWrite(segmentG, HIGH);
+  digitalWrite(dot, LOW);
+}
+
+void five() {
+  digitalWrite(segmentA, HIGH);
+  digitalWrite(segmentB, LOW);
+  digitalWrite(segmentC, HIGH);
+  digitalWrite(segmentD, HIGH);
+  digitalWrite(segmentE, LOW);
+  digitalWrite(segmentF, HIGH);
+  digitalWrite(segmentG, HIGH);
+  digitalWrite(dot, LOW);
+}
+
+void six() {
+  digitalWrite(segmentA, HIGH);
+  digitalWrite(segmentB, LOW);
+  digitalWrite(segmentC, HIGH);
+  digitalWrite(segmentD, HIGH);
+  digitalWrite(segmentE, HIGH);
+  digitalWrite(segmentF, HIGH);
+  digitalWrite(segmentG, HIGH);
+  digitalWrite(dot, LOW);
+}
+
+void seven() {
+  digitalWrite(segmentA, HIGH);
+  digitalWrite(segmentB, HIGH);
+  digitalWrite(segmentC, HIGH);
+  digitalWrite(segmentD, LOW);
+  digitalWrite(segmentE, LOW);
+  digitalWrite(segmentF, HIGH);
+  digitalWrite(segmentG, LOW);
+  digitalWrite(dot, LOW);
+}
+
+void eight() {
+  digitalWrite(segmentA, HIGH);
+  digitalWrite(segmentB, HIGH);
+  digitalWrite(segmentC, HIGH);
+  digitalWrite(segmentD, HIGH);
+  digitalWrite(segmentE, HIGH);
+  digitalWrite(segmentF, HIGH);
+  digitalWrite(segmentG, HIGH);
+  digitalWrite(dot, LOW);
+}
+
+void nine() {
+  digitalWrite(segmentA, HIGH);
+  digitalWrite(segmentB, HIGH);
+  digitalWrite(segmentC, HIGH);
+  digitalWrite(segmentD, HIGH);
+  digitalWrite(segmentE, LOW);
+  digitalWrite(segmentF, HIGH);
+  digitalWrite(segmentG, HIGH);
+  digitalWrite(dot, LOW);
 }
